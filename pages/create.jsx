29a4549438b5757980884c4307e4c5a20706258b
OlivierZiolkowski <olivier.ziolkowski@gmail.com>
2021-10-04 08:27:52 +0200

    Adding basic CreatePage component

---
+++ pages/create.jsx
@@ -0,0 +1,10 @@
+// Styles & assets
+import styles from "@styles/create.module.scss";
+
+export default function CreatePage() {
+    return (
+        <div className={styles.CreatePage}>
+            <h1>Créer un nouvel article</h1>
+        </div>
+    );
+}
