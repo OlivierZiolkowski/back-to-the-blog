8d420fe563574a0c462848ed71e52018aa3507e6
OlivierZiolkowski <olivier.ziolkowski@gmail.com>
2021-10-21 15:18:45 +0200

    auth context minor comments update

--- contexts/auth.js
+++ contexts/auth.js
@@ -1,8 +1,16 @@
+/**
+ * Auth declares a new context to authenticates user
+ * Its' contains an AuthProvider & a custom hook
+ */
+
+// Hooks / Functions
 import { createContext, useState, useEffect, useContext } from "react";
 import { onAuthStateChange } from "@lib/firebase";
 
+// Declare a new context for authentication
 const AuthContext = createContext({ user: null, userLoading: true });
 
+//* AuthProvider component
 export const AuthProvider = ({ children }) => {
     const [userLoading, setUserLoading] = useState(true);
     const [user, setUser] = useState();
@@ -21,4 +29,5 @@ export const AuthProvider = ({ children }) => {
     );
 };
 
+//* useAuth custom hook
 export const useAuth = () => useContext(AuthContext);
