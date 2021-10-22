2db03d249de7c763b396497bed276d3154425bf1
OlivierZiolkowski <olivier.ziolkowski@gmail.com>
2021-10-22 12:40:39 +0200

    Updating edit post form - TODO : correct picture upload

--- pages/edit/[slug].jsx
+++ pages/edit/[slug].jsx
@@ -1,3 +1,5 @@
+//! TODO - Fix image disappearing after update post
+
 /**
  * EditPage represents a page where
  * an authenticated user can edit a post.
@@ -11,8 +13,8 @@ import { useAuth } from "@contexts/auth";
 import { Layout } from "@components";
 import { getPostBySlug, updatePost } from "@lib/firebase";
 import { previewFile, slugifyTitle } from "@lib/utils";
-import { useFormik } from "formik";
-import * as Yup from "yup";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { EditPostValidationSchema } from "@lib/yup";
 
 // Assets & styles
 import styles from "@styles/edit.module.scss";
@@ -27,56 +29,6 @@ export default function EditPage({ post }) {
 
     // Check if user is authenticated
     const [user, userLoading] = useAuth();
-
-    // Formik parameters for update post form
-    const formik = useFormik({
-        initialValues: {
-            title: post.title,
-            category: post.category,
-            coverImage: null,
-            coverImageAlt: post.coverImageAlt,
-            content: post.content,
-            id: post.id,
-        },
-        // Validation schema
-        validationSchema: Yup.object({
-            title: Yup.string()
-                .max(100, "Le titre doit faire moins de 100 caractères")
-                .required("Veuillez renseigner un titre"),
-            category: Yup.string()
-                .oneOf(
-                    ["passé", "présent", "futur"],
-                    "Catégorie choisie invalide"
-                )
-                .required("Veuillez choisir une catégorie"),
-            coverImage: Yup.mixed(),
-            coverImageAlt: Yup.string().required(
-                "Veillez ajouter une description de l'illustration"
-            ),
-            content: Yup.string().required(
-                "Veuillez ajouter le contenu de votre article"
-            ),
-            id: Yup.string().required(),
-        }),
-        // Defines what's happened on form submission
-        onSubmit: (values) => {
-            setIsLoading(true);
-
-            const imageFile = document.getElementById("coverImage").files[0];
-            const imageName = slugifyTitle(values.title);
-
-            updatePost(values, imageFile, imageName)
-                .then(() => {
-                    setIsLoading(false);
-                    alert("Votre article a bien été mis à jour");
-                    router.push("/");
-                })
-                .catch((err) => {
-                    alert(err);
-                    setIsLoading(false);
-                });
-        },
-    });
 
     // Redirect to 404 if user is not authentified
     if (userLoading) {
@@ -98,112 +50,113 @@ export default function EditPage({ post }) {
             </Head>
             <Layout>
                 <div className={styles.EditPage}>
-                    <form onSubmit={formik.handleSubmit}>
-                        <h1>Éditer le post : {post.title}</h1>
+                    <h1>Éditer le post : {post.title}</h1>
+                    <Formik
+                        initialValues={{
+                            title: post.title,
+                            category: post.category,
+                            coverImage: null,
+                            coverImageAlt: post.coverImageAlt,
+                            content: post.content,
+                            id: post.id,
+                        }}
+                        validationSchema={EditPostValidationSchema}
+                        onSubmit={(values) => {
+                            setIsLoading(true);
 
-                        {/* Post title field */}
-                        <label htmlFor="title">Titre de l&apos;article</label>
-                        <input
-                            id="title"
-                            name="title"
-                            type="text"
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}
-                            value={formik.values.title}
-                        />
-                        {formik.touched.title && formik.errors.title ? (
-                            <div className="errorMessage">
-                                {formik.errors.title}
-                            </div>
-                        ) : null}
+                            const imageFile = document.querySelector(
+                                "input[name='coverImage']"
+                            ).files[0];
+                            const imageName = slugifyTitle(values.title);
 
-                        {/* Post category field */}
-                        <label htmlFor="category">Catégorie</label>
-                        <select
-                            id="category"
-                            name="category"
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}
-                        >
-                            <option value=""></option>
-                            <option value="passé">Passé</option>
-                            <option value="présent">Présent</option>
-                            <option value="futur">Futur</option>
-                        </select>
-                        {formik.touched.category && formik.errors.category ? (
-                            <div className="errorMessage">
-                                {formik.errors.category}
-                            </div>
-                        ) : null}
+                            updatePost(values, imageFile, imageName)
+                                .then(() => {
+                                    setIsLoading(false);
+                                    alert(
+                                        "Votre article a bien été mis à jour"
+                                    );
+                                    router.push("/");
+                                })
+                                .catch((err) => {
+                                    alert(err);
+                                    setIsLoading(false);
+                                });
+                        }}
+                    >
+                        {({ setFieldValue }) => (
+                            <Form>
+                                {/* Post title field */}
+                                <label htmlFor="title">
+                                    Titre de l&apos;article
+                                </label>
+                                <Field name="title" />
+                                <ErrorMessage
+                                    name="title"
+                                    render={(msg) => <div>{msg}</div>}
+                                />
 
-                        {/* Post cover image field */}
-                        <label htmlFor="coverImage">Illustration</label>
-                        <input
-                            id="coverImage"
-                            name="coverImage"
-                            type="file"
-                            accept="image/jpg"
-                            onChange={(event) => {
-                                formik.setFieldValue(
-                                    "file",
-                                    event.currentTarget.files[0]
-                                );
-                                previewFile();
-                            }}
-                            onBlur={formik.handleBlur}
-                            value={formik.values.coverImage}
-                        />
+                                {/* Post category field */}
+                                <label htmlFor="category">Catégorie</label>
+                                <Field as="select" name="category">
+                                    <option value=""></option>
+                                    <option value="passé">Passé</option>
+                                    <option value="présent">Présent</option>
+                                    <option value="futur">Futur</option>
+                                </Field>
+                                <ErrorMessage
+                                    name="category"
+                                    render={(msg) => <div>{msg}</div>}
+                                />
 
-                        {/* eslint-disable-next-line @next/next/no-img-element */}
-                        <img
-                            id="previewImg"
-                            src={post.coverImage}
-                            alt="preview"
-                        />
+                                {/* Post cover image field */}
+                                <label htmlFor="coverImage">Illustration</label>
+                                <Field
+                                    type="file"
+                                    name="coverImage"
+                                    accept="image/jpg"
+                                    onChange={(event) => {
+                                        setFieldValue(
+                                            "file",
+                                            event.currentTarget.files[0]
+                                        );
+                                        previewFile();
+                                    }}
+                                />
+                                {/* eslint-disable-next-line @next/next/no-img-element */}
+                                <img
+                                    id="previewImg"
+                                    src={post.coverImage}
+                                    alt="preview"
+                                />
+                                <ErrorMessage
+                                    name="coverImage"
+                                    render={(msg) => <div>{msg}</div>}
+                                />
 
-                        {formik.touched.coverImage &&
-                        formik.errors.coverImage ? (
-                            <div className="errorMessage">
-                                {formik.errors.coverImage}
-                            </div>
-                        ) : null}
+                                {/* Post cover image alt field */}
+                                <label htmlFor="coverImageAlt">
+                                    Description de l&apos;illustation
+                                </label>
+                                <Field name="coverImageAlt" />
+                                <ErrorMessage
+                                    name="coverImageAlt"
+                                    render={(msg) => <div>{msg}</div>}
+                                />
 
-                        {/* Post cover image alt field */}
-                        <label htmlFor="coverImageAlt">
-                            Description de l&apos;illustation
-                        </label>
-                        <input
-                            id="coverImageAlt"
-                            name="coverImageAlt"
-                            type="text"
-                            onChange={formik.handleChange}
-                            onBlur={formik.handleBlur}
-                            value={formik.values.coverImageAlt}
-                        />
-                        {formik.touched.coverImageAlt &&
-                        formik.errors.coverImageAlt ? (
-                            <div className="errorMessage">
-                                {formik.errors.coverImageAlt}
-                            </div>
-                        ) : null}
+                                {/* Post content field */}
+                                <label htmlFor="content">Contenu</label>
+                                <Field as="textarea" name="content" />
+                                <ErrorMessage
+                                    name="content"
+                                    render={(msg) => <div>{msg}</div>}
+                                />
 
-                        {/* Post content field */}
-                        <label htmlFor="content">Contenu</label>
-                        <textarea
-                            id="content"
-                            name="content"
-                            type="text"
-                            onChange={formik.handleChange}
-                            value={formik.values.content}
-                        />
-                        {formik.touched.content && formik.errors.content ? (
-                            <div className="errorMessage">
-                                {formik.errors.content}
-                            </div>
-                        ) : null}
-
-                        <button type="submit">Mettre à jour</button>
-                    </form>
+                                <button type="submit">
+                                    Mettre à jour l&apos;article
+                                </button>
+                            </Form>
+                        )}
+                    </Formik>
                 </div>
             </Layout>
         </>
